171438cd00631c945aa7b49299d4f771dae97c46
Jalajakshi D S <114593975+Jalajakshids20@users.noreply.github.com>
2024-01-23 21:49:54 +0530

    Server.js

---
+++ Server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Event listener for receiving messages from clients
+  socket.on('message', (message) => {
+    // Broadcast the received message to all connected clients
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
